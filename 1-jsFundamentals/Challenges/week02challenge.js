1a0d6c7c0b5400f51a71c80f8157275b363d4a44
Chris Blake <c.blake@Chriss-Air.attlocal.net>
2021-03-30 18:08:20 -0400

    Adding additional notes

---
+++ 1-jsFundamentals/Challenges/week02challenge.js
@@ -0,0 +1,17 @@
+//* Challenge:
+//* Create a variable named FB that takes numbers
+//* Write a conditional that: 
+//* Prints out "Fizz" if the number is divisible by 3
+//* Prints out "Buzz" if the number is divisible by 5
+//* Prints out "Fizz Buzz" if the number is divisible by BOTH 3 and 5
+//* Bronze: Write as a If else
+//* Silver: Write as a Switch,
+//* Gold: Write as a Ternary
+
+
+
+// Gold
+for (let i = 0; i <= 100; i++){
+    i % 3 == 0 && i % 5 == 0 ? console.log("Fizz Buzz") : i % 3 == 0 ? console.log("Fizz") : i % 5 == 0 ? console.log("Buzz") : 
+    console.log(i);
+}
